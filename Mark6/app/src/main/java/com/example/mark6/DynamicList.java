86fb060f573eddb203e50701fadf1d31d7853c09
schaud3 <schaudhury@walmartlabs.com>
2019-04-27 21:22:43 +0530

    backup 1

---
+++ Mark6/app/src/main/java/com/example/mark6/DynamicList.java
@@ -0,0 +1,13 @@
+package com.example.mark6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DynamicList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic_list);
+    }
+}
